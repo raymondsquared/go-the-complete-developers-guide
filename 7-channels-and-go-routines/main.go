554178c7816479bbe99e01dbd53567a38b65d41a
raymondsquared <bolesray@outlook.com>
2024-08-05 10:57:09 +1000

    feat: advanced channel
    do not share variables between different go routines, instead pass values
    use literal function (lambda)

--- 7-channels-and-go-routines/main.go
+++ 7-channels-and-go-routines/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,8 +20,12 @@ func main() {
 	for _, link := range links {
 		go checkLink(link, c)
 	}
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+
+	for l := range c {
+		go func(link string) {
+			time.Sleep(5 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
 }
 
@@ -28,10 +33,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	c <- link
 }
